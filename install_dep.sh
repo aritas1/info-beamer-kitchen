66aea6faaa966bec6af5a0f79b31b4a098ec42ad
Beamer User <beamer@lumos.(none)>
2013-02-13 21:55:13 +0100

    add run and init.d script, fix missing make

--- install_dep.sh
+++ install_dep.sh
@@ -7,8 +7,13 @@ git clone https://github.com/aritas1/barcodescanner.git ../barcodescanner
 echo ">>>>>>>>>>>>>>> Make BarcodeScanner <<<<<<<<<<<<<<<<<"
 make -C ../barcodescanner/
 
+
 echo ">>>>>>>>>>>>>>> Cloning Info-Beamer <<<<<<<<<<<<<<<<<"
 git clone https://github.com/aamuuninen/info-beamer ../info-beamer
+
+echo ">>>>>>>>>>>>>>> Make Info_Beaner <<<<<<<<<<<<<<<<<"
+make -C ../info-beamer/
+
 
 echo ">>>>>>>>>>>>>>> Cloning Info-Beamer-Nodes <<<<<<<<<<<<<<<<<"
 git clone https://github.com/aamuuninen/info-beamer-nodes ../info-beamer-nodes
--- update_all.sh
+++ update_all.sh
@@ -4,11 +4,13 @@ echo ">>>>>>>>>>>>>>> Updating BarcodeScanner <<<<<<<<<<<<<<<<<"
 cd ../barcodescanner
 git pull
 echo ">>>>>>>>>>>>>>> Updating c program (make) <<<<<<<<<<<<<<<<<"
-make 
+make
 
 echo ">>>>>>>>>>>>>>> Updating Info-Beamer <<<<<<<<<<<<<<<<<"
 cd ../info-beamer
 git pull
+echo ">>>>>>>>>>>>>>> Updating c program (make) <<<<<<<<<<<<<<<<<"
+make
 
 echo ">>>>>>>>>>>>>>> Updating Info-Beamer-Node <<<<<<<<<<<<<<<<<"
 cd ../info-beamer-nodes
