ef28fb91e91ae801ea34d2c3ed37ff1f7ba1c68e
Beamer User <beamer@lumos.(none)>
2013-02-15 02:05:34 +0100

    update stop script

--- stop.sh
+++ stop.sh
@@ -2,5 +2,7 @@
 
 # kill all pssible running programs
 killall weather_updater.sh > /dev/null 2>&1
+killall photo_syn.sh > /dev/null 2>&1
 killall info-beamer > /dev/null 2>&1
 killall scanner.jar > /dev/null 2>&1
+
