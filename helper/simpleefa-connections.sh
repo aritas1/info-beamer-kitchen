949d35a61cef8245b97360b586f8ec8f5cdb98f1
Beamer User <beamer@lumos.(none)>
2013-02-20 21:41:27 +0100

    add sleep to simpleefa helper to avoid fails

--- helper/simpleefa-connections.sh
+++ helper/simpleefa-connections.sh
@@ -2,6 +2,9 @@
 ../apache-tomcat-7.0.35/bin/shutdown.sh
 ../apache-tomcat-7.0.35/bin/startup.sh
 
+
+sleep 15
+
 while :
 do
 wget --output-document tmp 'http://localhost:8080/simpleefa/connection?from=6906707&to=6906508'
