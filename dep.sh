302edc963b11d8f417f7ba65789e1851acaa18c9
beamer <beamer@lumos.(none)>
2013-03-04 01:49:50 +0100

    add deb.sh

---
+++ dep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install gcc g++ make liblua5.1-dev libevent-dev libglfw-dev libglew1.5-dev libftgl-dev libavcodec-dev libswscale-dev libavformat-dev libdevil-dev lua5.1 openjdk-6-jdk
